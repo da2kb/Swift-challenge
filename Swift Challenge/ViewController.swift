26c6a86f637580ec06ee8b8f478a193994d508af
Dina Ahmed <da2kb@mail.missouri.edu>
2019-09-07 00:26:02 -0500

    Initial Commit

---
+++ Swift Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Challenge
+//
+//  Created by Dina Adel on 9/7/19.
+//  Copyright © 2019 Dina Adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift ChallengeTests/Swift_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_ChallengeTests.swift
+//  Swift ChallengeTests
+//
+//  Created by Dina Adel on 9/7/19.
+//  Copyright © 2019 Dina Adel. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Challenge
+
+class Swift_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
